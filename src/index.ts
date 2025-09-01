3d4cbe5678deb0de581b2d97ccbd14fc8c848f50
Daniel Henrique Sell <kanee.sell@gmail.com>
2025-09-01 15:01:07 -0300

    Fix: Ping usando Axios -> Ping usando a lib 'ping' não ativa backend na render, e não pode conter 'https://', axios.post ativa backend na render e pode conter 'https://'

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,44 @@
-import ping from "ping";
 import chalk from "chalk";
+import axios from "axios";
+import "dotenv/config";
 
-const minutos = 14;
-const urlApi = process.env.URL_API || ""
+const minutos = Number(process.env.TEMP_PING_MIN) || 10; // Tempo Default 10min -> configurar no .env
+const urlApi = process.env.URL_API || ""; // Sem URL Default -> Configurar no .env
 
 const calcTemp = (temp: number) => temp * 60000;
 
 async function execultarPing() {
-  ping.promise.probe(urlApi).then((res) => {
-    if (res.alive !== false) {
-      console.log(
-        `${chalk.green("Ping:")} ${chalk.yellow("{")} ${chalk.blue("Host:")} ${
-          chalk.green(res.host)
-        }, ${chalk.blue("Ativo?:")} ${res.alive? chalk.green("Sim"): chalk.red("Não")}, ${chalk.blue("Tempo:")} ${
-          chalk.green(res.time)
-        }, ${chalk.blue("Data e Hora:")} ${chalk.green(new Date().toLocaleDateString() + " - " + new Date().toLocaleTimeString())} ${chalk.yellow("}")}`
-      );
-    } else {
-      console.log(chalk.red("Ping Falhou..."));
-    }
-  });
+  if (urlApi === "") return console.log(chalk.red("Sem urlApi..."));
+  const timeStart = Date.now();
+  const res = await axios.post(urlApi);
+  const timeEnd = Date.now();
+  const resTime = timeEnd - timeStart;
+  console.log(
+    `${chalk.blue(
+      `Alvo: { name: ${urlApi}, hora: ${new Date().toLocaleTimeString()} }`
+    )}`
+  );
+  if (res.status !== 200) {
+    return console.log(
+      `${chalk.green("Ping:")} ${chalk.yellow("{")} ${chalk.blue("Ativo?:")} ${
+        res.status === 200 || res.status === 201
+          ? chalk.green("Sim")
+          : chalk.red("Não")
+      }, ${chalk.blue("Tempo:")} ${chalk.green(`${resTime}ms`)}, ${chalk.blue(
+        "Data e Hora:"
+      )} ${chalk.green(
+        new Date().toLocaleDateString() +
+          " - " +
+          new Date().toLocaleTimeString()
+      )} ${chalk.yellow("}")}`
+    );
+  } else {
+    return console.log(chalk.red("Ping Falhou..."));
+  }
 }
 console.log(chalk.bold.green("-- [ AUTO-PING ] --\n"));
 console.log(chalk.blue("Iniciando..."));
+console.log(chalk.black.bgWhite(`[ Ping a cada ${minutos}min ]`));
 execultarPing();
 
 setInterval(() => {
